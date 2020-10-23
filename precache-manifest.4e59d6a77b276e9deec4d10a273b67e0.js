8961d8a1c402854e44f96f4787f3c1d615f4dc31
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2020-10-23 21:06:27 +0200

    Updates

---
+++ precache-manifest.4e59d6a77b276e9deec4d10a273b67e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccdd845041d99854ede6d4cca791841",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "6bc7407a24e3ea4c49bc",
+    "url": "/covid-tracker/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "20a36b5a197fad91097b",
+    "url": "/covid-tracker/static/css/main.9059f345.chunk.css"
+  },
+  {
+    "revision": "6bc7407a24e3ea4c49bc",
+    "url": "/covid-tracker/static/js/2.f0c8f98d.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/covid-tracker/static/js/2.f0c8f98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a36b5a197fad91097b",
+    "url": "/covid-tracker/static/js/main.943ed77f.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
